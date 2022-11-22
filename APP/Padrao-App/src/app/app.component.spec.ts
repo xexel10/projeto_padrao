5d803ad1e9c131d5505bedafcbe14978ca3a077d
Fabiano Amorim <fabiano_amorim@hotmail.com>
2022-11-22 09:30:04 -0300

    Teste

--- APP/Padrao-App/src/app/app.component.spec.ts
+++ APP/Padrao-App/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Padrao-App'`, () => {
+  it(`should have as title 'padrao-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Padrao-App');
+    expect(app.title).toEqual('padrao-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Padrao-App app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('padrao-app app is running!');
   });
 });
--- APP/Padrao-App/src/app/app.module.ts
+++ APP/Padrao-App/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
